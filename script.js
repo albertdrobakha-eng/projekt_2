6c7cf86e5f6eeb296ef39d67953b51688ddb066c
albertdrobakha-eng <albertdrobakha@gmail.com>
2026-03-05 11:26:27 +0200

    Update files

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 console.log("hello world")
 
  
+ const messageInput = document.querySelector('#messageInput');
+ const sendBtn = document.querySelector('#sendBtn');
+ const messageList = document.querySelector('#messageList');
+
+ async function getMessages() {
+ 	const messageData = { text: 'Hello!' };
+
+ 	const response = await fetch('script.js', {
+ 		method: 'POST',
+ 		headers: {
+ 			'Content-Type': 'application/json'
+ 		},
+ 		body: JSON.stringifty(messageData)
+ 	});
+ 	const result = await response.json();
+ 	
+ }
+
