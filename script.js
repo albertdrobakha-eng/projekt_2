9444821d70dfe362ea353c26fea044aa2f755330
albertdrobakha-eng <albertdrobakha@gmail.com>
2026-03-12 11:15:17 +0200

    Update files

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-console.log("hello world")
-
  
  const messageInput = document.querySelector('#messageInput');
  const sendBtn = document.querySelector('#sendBtn');
  const messageList = document.querySelector('#messageList');
 
+async function getMessages() {
+	const response = await fetch('http://tinkr.tech/sdb/projekt_2');
+	const messages = await response.json();
+
+  messageList.innerHTML = '';
+  messages.forEach(msg => {
+    const messageDiv = document.createElement('div');
+    messageDiv.className = 'message';
+    messageDiv.textContent = msg.text;
+    messageList.appendChild(messageDiv);
+  });
+}
+
+
+
  async function getMessages() {
+
+const text = messageInput.value;
+
  	const messageData = { text: 'Hello!' };
 
  	const response = await fetch('http://tinkr.tech/sdb/projekt_2', {
+ 		//const messages = await response.json(); 
  		method: 'POST',
  		headers: {
  			'Content-Type': 'application/json'
@@ -16,7 +33,15 @@ console.log("hello world")
  		body: JSON.stringifty(messageData)
  	});
  	const result = await response.json();
- 	
+
+ 	messageInput.value = '';
+ 	getMessages();
  }
+
+sendBtn.addEventListener('click',sendMessage);
+
+getMessages();
+ 	
  
+
 
